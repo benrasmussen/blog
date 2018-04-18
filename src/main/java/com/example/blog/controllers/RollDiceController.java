65ee07ca1463e304a05421e3836fb5433495b242
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-04-17 20:14:56 -0500

    finished with dice game using spring

--- src/main/java/com/example/blog/controllers/RollDiceController.java
+++ src/main/java/com/example/blog/controllers/RollDiceController.java
@@ -5,28 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class RollDiceController {
 
-
     @GetMapping("/roll-dice")
-    public String showPage(){
+    public String showGame() {
         return "roll-dice";
     }
 
     @GetMapping("/roll-dice/{guess}")
-    public String play(@PathVariable Integer guess, Model vModel){
-        // Generating a rnd number from 1 to 6
-        int rnd = (int) (Math.random() * 6 + 1);
-
-        // Determines if you win or not
-        boolean win = (rnd == guess) ? true : false;
-
-        vModel.addAttribute("guess", guess);  // request.setAttribute("guess", guess);
-        vModel.addAttribute("rnd", rnd);
-        vModel.addAttribute("win", win);
-
-        return "roll-dice";
+    public String play(@PathVariable int guess, Model model) {
+        int rnd = (int )(Math.random() * 6 + 1);
+        boolean res = (guess == rnd);
+        model.addAttribute("result", res);
+        model.addAttribute("number", rnd);
+        return "dice-outcome";
     }
 
 }
