f67d4b8bc1d8ec6fe65a18028b0da1a0add1256f
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-04-17 21:23:26 -0500

    finished blog exercise

--- src/main/java/com/example/blog/controllers/PostController.java
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -1,11 +1,33 @@
 package com.example.blog.controllers;
 
+import com.example.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class PostController {
+
+    @GetMapping("/posts/index")
+    public String index(Model mod) {
+        List<Post> posts = Post.all();
+        mod.addAttribute("posts", posts);
+        return "posts/index";
+    }
+
+    @GetMapping("/posts/show")
+    public String individualPost(Model mod) {
+        Post post = Post.myPost();
+        String title = post.getTitle();
+        String body = post.getBody();
+        mod.addAttribute("title", title);
+        mod.addAttribute("body", body);
+        return "posts/show";
+    }
+
     @GetMapping("/posts")
     @ResponseBody
     public String index() {
@@ -21,7 +43,7 @@ public class PostController {
     @GetMapping("/posts/create")
     @ResponseBody
     public String insert() {
-        return "INSERT - 4view the form for creating a post";
+        return "INSERT - view the form for creating a post";
     }
 
     @PostMapping("/posts/create")
---
+++ src/main/java/com/example/blog/models/Post.java
@@ -0,0 +1,52 @@
+package com.example.blog.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+
+    private String title;
+    private String body;
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public static Post myPost() {
+        String title = "Post numero uno!";
+        String body = "This is my very first post! Isn't it grand!";
+        return new Post(title, body);
+    }
+
+    public static Post myPost2() {
+        String title = "Another amazing post";
+        String body = "Man this is just fantastic";
+        return new Post(title, body);
+    }
+
+    public static List<Post> all() {
+        List<Post> posts = new ArrayList<>();
+        posts.add(myPost());
+        posts.add(myPost2());
+        return posts;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
+
+
