3c8511d17abf0a3ebd9ee7b0883c045e79a1fc40
Ben Rasmussen <Benrasmussen11@gmail.com>
2018-04-24 12:23:18 -0500

    styled and added further functionality to springboot blog

--- src/main/java/com/example/blog/controllers/UserController.java
+++ src/main/java/com/example/blog/controllers/UserController.java
@@ -19,13 +19,13 @@ public class UserController {
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping("/sign-up")
+    @GetMapping("/signup")
     public String showSignupForm(Model model){
         model.addAttribute("user", new User());
         return "users/sign-up";
     }
 
-    @PostMapping("/sign-up")
+    @PostMapping("/signup")
     public String saveUser(@ModelAttribute User user){
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
